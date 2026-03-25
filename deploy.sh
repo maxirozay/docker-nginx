13047f59ddd92fdd162e9bfee78005f10691b3e8
Maxime Rossier <rossier.maxime@gmail.com>
2026-03-25 16:54:45 +0100

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+set -a
+if [ -f ".env" ]; then
+  . ".env"
+fi
+set +a
 
 NAME="nginx"
-SERVER_URL="debian@<ip>"
-SSH_KEY="~/.ssh/<key>"
-REMOTE_PATH="~/"
+SERVER_URL="$SERVER_URL"
+SSH_KEY="~/.ssh/$SSH_KEY_NAME"
+REMOTE_PATH="~/nginx"
+echo "Deploying to $SERVER_URL with SSH key $SSH_KEY_NAME"
 
 ssh -i $SSH_KEY $SERVER_URL "mkdir -p $REMOTE_PATH"
-scp -i $SSH_KEY -r $(pwd) $SERVER_URL:$REMOTE_PATH
+scp -i $SSH_KEY -r $(pwd)/nginx $SERVER_URL:$REMOTE_PATH
+scp -i $SSH_KEY -r $(pwd)/sites $SERVER_URL:$REMOTE_PATH
+scp -i $SSH_KEY -r $(pwd)/compose.yaml $SERVER_URL:$REMOTE_PATH
+scp -i $SSH_KEY -r $(pwd)/README.md $SERVER_URL:$REMOTE_PATH
 ssh -i $SSH_KEY $SERVER_URL "
-  cd ~/$NAME && \
+  cd $REMOTE_PATH && \
   sudo docker compose up -d
 "
